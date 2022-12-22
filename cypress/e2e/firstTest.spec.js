39aec0a1ab85e14556a7e74aa1d7b0147b531744
Andrew Davis <andrew4d3@gmail.com>
2022-12-22 08:39:50 -0300

    Lecture 12

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,37 +2,58 @@
 
 /* What are we doing here? */
 describe("Our first suite", () => {
-    it("first test", () => {
-        cy.visit("/")
-        cy.contains('Forms').click()
-        cy.contains('Form Layouts').click()
+  it("first test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
 
-        // by tag name
-        cy.get('input')
+    // by tag name
+    cy.get("input");
 
-        // by Id
-        cy.get('#inputEmail1')
+    // by Id
+    cy.get("#inputEmail1");
 
-        // by class name
-        cy.get('.input-full-width')
+    // by class name
+    cy.get(".input-full-width");
 
-        // by attribute name (without value)
-        cy.get('[placeholder]')
+    // by attribute name (without value)
+    cy.get("[placeholder]");
 
-        // by attribute name (with value)
-        cy.get('[placeholder="Email"]')
+    // by attribute name (with value)
+    cy.get('[placeholder="Email"]');
 
-        // by class value
-        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    // by class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
 
-        // by tag name with email atributte with value
-        cy.get('input[placeholder="Email"]')
+    // by tag name with email atributte with value
+    cy.get('input[placeholder="Email"]');
 
-        // by two different attributes
-        cy.get('[placeholder="Email"][type="email"]')
+    // by two different attributes
+    cy.get('[placeholder="Email"][type="email"]');
 
-        // The most recommended way in cypress
-        cy.get('[data-cy="imputEmail1"]')
-    })
-})
+    // The most recommended way in cypress
+    cy.get('[data-cy="imputEmail1"]');
+  });
 
+  it.only("second test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    /* Explain what each of these instructions do */
+    cy.get('[data-cy="signInButton"');
+    cy.contains("Sign in");
+
+    cy.contains('[status="warning"]', "Sign in");
+
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+
+    cy.contains("nb-card", "Horizontal form").find('[type="email"]');
+  });
+});
